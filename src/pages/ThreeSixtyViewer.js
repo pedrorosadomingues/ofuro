f9b5c89e174511e079884fc272dbae5001d8df85
Pedro Rosa <pedrorosadomingues@gmail.com>
2024-06-19 21:22:46 -0300

    Adjust initial rotation of cover image in ThreeSixtyViewer

--- src/pages/ThreeSixtyViewer.js
+++ src/pages/ThreeSixtyViewer.js
@@ -44,7 +44,7 @@ const ThreeSixtyViewer = () => {
       const mesh = new THREE.Mesh(geometry, material);
 
       // Ajustar a rotação inicial da imagem CAPA
-      mesh.rotation.y = THREE.MathUtils.degToRad(-45);
+      mesh.rotation.y = THREE.MathUtils.degToRad(-75);
 
       scene.add(mesh);
     });
